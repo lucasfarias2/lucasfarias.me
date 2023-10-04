0aaf8a2ca8d492e364be1c1a02776ba6b2f22bfd
Lucas Farias <lukf95@gmail.com>
2023-10-04 22:34:05 +0200

    Revert

--- src/components/HomeItem.jsx
+++ src/components/HomeItem.jsx
@@ -40,7 +40,7 @@ const HomeItem = ({ title, description, bgColorClass, website, imgSrc, Icon, git
         <div>
           {github && (
             <a
-              href={website}
+              href={github}
               target="_blank"
               rel="noreferrer"
               className={classnames(
@@ -53,7 +53,7 @@ const HomeItem = ({ title, description, bgColorClass, website, imgSrc, Icon, git
           )}
 
           <a
-            href={github}
+            href={website}
             target="_blank"
             rel="noreferrer"
             className="text-black font-medium p-2 border rounded-lg text-sm hover:bg-black hover:text-white flex items-center justify-center"
